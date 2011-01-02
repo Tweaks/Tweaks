f1ce28a2bc4e2be3c606da3a276f13bdd0cd3fc5
Tweaks Blackboard Building Block <tweaks@boxc.net>
2011-01-03 06:45:38 +1000

    optimisations in header searching from submenu

--- Source/WebContent/scripts/_x/jquery_dynamicresourcetable.js
+++ Source/WebContent/scripts/_x/jquery_dynamicresourcetable.js
@@ -91,20 +91,20 @@ jQuery(function($){
 			// normal table processing
 			for (var column = 0; column < resourceTypes.length; column++) {
 				if (sectionTitle.length && resourceTypes[column].length) {
-					headers.filter(":contains('"+sectionTitle+": "+resourceTypes[column]+"')").each(function(matchingHeader) {
+					headers.filter(":contains('"+sectionTitle+": "+resourceTypes[column]+"')").each(function() {
 
 						if(location.href.indexOf("Content.")>0)
-							$(matchingHeader).parents(tweak_bb.row_element).hide();
+							$(this).parents(tweak_bb.row_element).hide();
 						
 						var thiscell = $unitMap.find("tr:eq("+(row+1)+") td:eq("+(column+columnOffset)+")");
 
 						// set up link (if there is one): consider / is there better way that deals with filtering edit mode links ok
-						var thislink = $(matchingHeader).find("a:contains('"+sectionTitle+": "+resourceTypes[column]+"')").clone();
+						var thislink = $(this).find("a:contains('"+sectionTitle+": "+resourceTypes[column]+"')").clone();
 						if (thislink.length)
 							thislink = setUpLinkOptions(thislink, thiscell, displayLinkTopicIndexText, displayLinkResourceText, sectionTitle, resourceTypes[column]);
 					
 						// find details item
-						var details = $(matchingHeader).parents(tweak_bb.row_element).children("div.details");
+						var details = $(this).parents(tweak_bb.row_element).children("div.details");
 						
 						// attachments (reinsert trailing space outside links)
 						var attachments = details.find("ul.attachments a").clone().each(function() {
