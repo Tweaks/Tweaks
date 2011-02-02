c1a444557afd9b5f5ea201ba9f02c40935774cf1
Tweaks Blackboard Building Block <tweaks@boxc.net>
2011-02-02 22:42:20 +1000

    2011 date update

--- Source/WebContent/scripts/_x/jquery_hideshow.js
+++ Source/WebContent/scripts/_x/jquery_hideshow.js
@@ -1,5 +1,5 @@
 /* 
-   Copyright 2010 Tim Plaisted, Queensland University of Technology
+   Copyright 2011 Tim Plaisted, Queensland University of Technology
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
