fa131588dfed72672b79a8cd6126b83587832319
Tweaks <tweaks@boxc.net>
2011-03-04 09:23:00 +1000

    rewrote image path to use path setting

--- Source/WebContent/scripts/_x/jquery_selfenrol.js
+++ Source/WebContent/scripts/_x/jquery_selfenrol.js
@@ -5,8 +5,8 @@
  * Licensed under the MIT License: http://www.opensource.org/licenses/mit-license.php
 */
 // use server path for github hosting or server relative path for building block
-var remoteHost = (jQuery("script.tweak_script").parents("div.details:contains(\"Self Enrol\")").filter(":contains(\"github\")").length>0);
-var tb_pathToImage = (remoteHost ? "http://tweaks.github.com/Tweaks/Source/WebContent" : "/webapps/qut-tweakbb-bb_bb60") + "/images/loadingAnimation.gif";
+var tweak_imagePath = (window.tweak_path == null) ? "../" : ((tweak_path == "") ? "http://tweaks.github.com/Tweaks/Source/WebContent/" : tweak_path) + "images/";
+var tb_pathToImage = tweak_imagePath + "loadingAnimation.gif";
 /*!!!!!!!!!!!!!!!!! edit below this line at your own risk !!!!!!!!!!!!!!!!!!!!!!!*/
 //on page load call tb_init :: customised with functions at end
 //add thickbox to href & area elements that have a class of .thickbox
