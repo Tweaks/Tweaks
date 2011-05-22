9f1648f3fb97d9afc736fc9f1a60e407ff5bc670
Tweaks Blackboard Building Block <tweaks@boxc.net>
2011-05-22 18:32:48 +0200

    adding rollover support to image maps

--- Source/WebContent/scripts/_x/jquery_dynamicimagemaplinks.js
+++ Source/WebContent/scripts/_x/jquery_dynamicimagemaplinks.js
@@ -28,6 +28,18 @@ jQuery(function($){
   var headers = $(tweak_bb.page_id +" > "+tweak_bb.row_element).children(".item");
   var descItem = $("#description, .description");
 
+  // 9.x utility functions to preload images
+  var buildPreload = function(src, index) {
+	return (jQuery.browser.mozilla) ?
+		"<iframe src=\""+src+"\" name=\""+index+"\" width=\"55\" height=\"55\"></iframe>" :
+		"<img src=\""+src+"\" id=\""+index+"\" width=\"55\" height=\"55\">";
+  };
+  var getPreloadURL = function(url, framename) {
+	return (jQuery.browser.mozilla) ? 
+		frames[framename].window.location.href :
+		url;
+  };
+
   // check if rollover images set up required (
   var hasRollovers = false, rolloverItems, preloadrolloverHTML = "";
   var rolloverRow = headers.filter(":contains('Rollover Images'):first").parents(tweak_bb.row_element);
@@ -39,7 +51,8 @@ jQuery(function($){
   $(tweak_bb.page_id+" map").each(function() {
 	// parse each map
     var thisMapName = $(this).attr("name");
-    var thisBaseImageSrc = $("img[usemap*=\""+thisMapName+"\"]").attr("src");
+    var thisMapImage = $("img[usemap*=\""+thisMapName+"\"]");
+    var thisMapImageSrc = $(thisMapImage).attr("src");
     
     // parse each maps area tags
 	$(this).children().each(function(index, areaItem) {
@@ -77,8 +90,11 @@ jQuery(function($){
 					var this_href= matchingRolloverItem.attr("href");
 					if (this_href.search("href=") > -1)
 						this_href = unescape(this_href.substr(this_href.search("href=")+5, this_href.length)).replace("amp;", ""); 
-					$(this).mouseover(function(){ jQuery("img[usemap*=\""+thisMapName+"\"]").attr("src", this_href); });
-					preloadrolloverHTML += "<img src=\""+this_href+"\" width=\"55\" height=\"55\" alt=preloadimage>";
+					var this_rolloverid = thisMapName+"_"+index;
+					preloadrolloverHTML += buildPreload(this_href, this_rolloverid);
+					$(this).mouseover(function(){
+						thisMapImage.attr("src", getPreloadURL(this_href, this_rolloverid));
+					});
 				}
 			}
 		}
@@ -86,11 +102,15 @@ jQuery(function($){
 	
 	// add preload images and attach mouseout event
 	if (hasRollovers) {
+		var this_rolloverid = thisMapName+"_"+0;
+		preloadrolloverHTML += buildPreload(thisMapImageSrc, this_rolloverid);
 	  	rolloverRow.find("div.details").append(preloadrolloverHTML);
-		$(this).children().mouseout(function(){ jQuery("img[usemap*=\""+thisMapName+"\"]").attr("src", thisBaseImageSrc); });
+		$(this).children().mouseout(function(){ 
+			thisMapImage.attr("src", getPreloadURL(thisMapImageSrc, this_rolloverid));
+		});
 	}
   });
-  
+    
   // attach description event
   if (descItem.length) {
 	$("area").mouseover(function(){
